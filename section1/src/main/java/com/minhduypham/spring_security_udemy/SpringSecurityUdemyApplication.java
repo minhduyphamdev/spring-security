eb258ba7983f40d288fc39441cbc39cda6159da1
minh pham <hadtnt71@gmail.com>
2023-11-01 20:52:25 +0700

    first commit

---
+++ section1/src/main/java/com/minhduypham/spring_security_udemy/SpringSecurityUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.minhduypham.spring_security_udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityUdemyApplication.class, args);
+	}
+
+}
---
+++ section1/src/test/java/com/minhduypham/spring_security_udemy/SpringSecurityUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhduypham.spring_security_udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
